bbcac800e2f28997f9e47f6a0cf7c8c74d2cb50b
Duke6611 <17766117052@163.com>
2023-01-29 16:52:27 +0800

    [新增：] 2023.01.29 第一次提交

---
+++ src/main/java/com/duke/test/ApplysTechs.java
@@ -0,0 +1,13 @@
+package com.duke.test;
+
+/**
+ * @Auther: songsiyuan
+ * @Date: 2023/1/29 - 01 - 29 - 16:49
+ * @Description: com.duke.test
+ * @version: 1.0
+ */
+public class ApplysTechs {
+    public static void main(String[] args) {
+        System.out.println("hello git! hello duke");
+    }
+}
